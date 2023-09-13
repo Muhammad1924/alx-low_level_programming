38bc13778abaa9104817e8978259a023e1b7add5
Muhammad1924 <muhammadolaitan1924@gmail.com>
2023-09-13 14:33:33 -0700

    BEST

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - main function
+ * print_alphabet_x10 - main function
  *
  * Return: void
  */
@@ -18,5 +18,4 @@ void print_alphabet_x10(void)
 	}
 
 _putchar('\n');
-return (0);
 }
